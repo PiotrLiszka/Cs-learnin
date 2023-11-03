f730f9d34593965303d2aed3534c2f7adcc3943b
PiotrLiszka <piotr.liszka.krk@gmail.com>
2023-11-03 21:56:32 +0100

    str 95

--- roz 01/yoyo_cs/Program.cs
+++ roz 01/yoyo_cs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace yoyo_cs
 {
@@ -6,7 +8,29 @@ namespace yoyo_cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine(string.Join(",", args));
+            Assembly? zestaw = Assembly.GetEntryAssembly();
+            if (zestaw == null) return;
+
+            foreach (AssemblyName nazwa in zestaw.GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(nazwa);
+                int liczbaMetod = 0;
+
+                foreach(var t in a.DefinedTypes)
+                {
+                    liczbaMetod += t.GetMethods().Count();
+                }
+
+                Console.WriteLine(
+                    "W zestawie {0} jest {1:N0} typów i {2:N0} metod.",
+                    arg0: nazwa.Name,
+                    arg1: a.DefinedTypes.Count(),
+                    arg2: liczbaMetod
+                );
+            }
+
         }
     }
 }
